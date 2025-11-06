55cc851271a0d1705a3faa435b00d1324b8670ce
Kosawat Sukchaya <kosawat@gmail.com>
2025-11-06 10:53:16 +0100

    Initialized billing page and topup modal

--- src/app/(private)/dashboard/billing/page.tsx
+++ src/app/(private)/dashboard/billing/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
+import TopUpModal from "@/components/billing/TopUpModal";
+import { useUser } from "@/hooks/useUser";
+import { useState } from "react";
+
 export default function BillingPage() {
-  function handleTopup() {
-    console.log("Topup Clicked");
-  }
+  const { userId } = useUser();
+  const [records, setRecords] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="p-6 text-white bg-[#050505] min-h-screen">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-semibold">Billing and Credits</h1>
         <button
-          onClick={handleTopup}
+          onClick={() => setShowModal(true)}
           className="px-4 py-2 bg-[#191919] hover:bg-[#232323] border border-[#2b2b2b] rounded-lg"
         >
           + Top Up
@@ -17,9 +22,9 @@ export default function BillingPage() {
       </div>
 
       <div>Billing History Table</div>
-      {/* <BillingHistoryTable records={records} />
+      {/* <BillingHistoryTable records={records} /> */}
 
-      {showModal && <TopUpModal onClose={() => setShowModal(false)} />} */}
+      {showModal && <TopUpModal onClose={() => setShowModal(false)} />}
     </div>
   );
 }
